875406e271fd7cd0ca09f82e5eaaacba343cff04
HassanBadr <hassandev90@gmail.com>
2018-09-13 15:15:29 +0200

    update

---
+++ onlineshoppingprod/src/main/java/com/peace/onlineshoppingprod/controller/PageController.java
@@ -0,0 +1,30 @@
+package com.peace.onlineshoppingprod.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/","/home","/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","welcome to spring mvc");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greetingmsg",required=false) String greeting) {
+		if(greeting == null) {
+			greeting = "Hello There";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		
+		return mv;
+	}
+
+}
